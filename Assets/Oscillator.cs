b10ccadb2a6f15221da0d35cd40c3d1845fd5951
Marija Isupova <marija.isupova@gmail.com>
2020-12-06 21:46:31 +0200

    Add oscillating object to Level 1

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 0f, 0f);
+    [SerializeField] float period = 2.5f;
+    
+    [Range(0, 1)] [SerializeField] float movementFactor; // 0 for not moved 1 for fully moved;
+    Vector3 startingPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period; // grows continually from 0
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+        movementFactor = rawSinWave / 2f + 0.5f;
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
